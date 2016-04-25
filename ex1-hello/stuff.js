2fb5ab55a647dea69a83c55d264b713404756be2
Josue Fontecha <jfonte@users.noreply.github.com>
2016-04-25 05:02:24 +0200

    added concats just because im cool like that :)

--- ex1-hello/stuff.js
+++ ex1-hello/stuff.js
@@ -1,10 +1,19 @@
 function myJSfunction(){
+	var str1 = "<p> > Welcome commander Ripley. It has been a long time. </p>";
+	var str2 = "<p>> My records show your last entry was on June 6th, 2122.</p>";
+	var str3 = "<p>> How can I be of assistance?</p>";
+	var res = str1.concat(str2,str3);
     var x = document.getElementById('input1').value;
     if (x == "ripley"){
-        document.getElementById("reset").innerHTML = "<p> > Welcome commander Ripley. It has been a long time. </p><p>> My records show your last entry was on June 6th, 2122.</p><p>> How can I be of assistance?</p>"
+        document.getElementById("reset").innerHTML = res;
         // var reply = "> Welcome commander Ripley"
     } else {
-        document.getElementById("reset").innerHTML = "<p class='error'> > Please try again. </p>"
+    	var err1 = "<p class='error'> > Your ID: '"
+    	var err2 = "' is unknown. Login will reset in 3 seconds... </p>"
+    	var error = err1.concat(x,err2);
+        document.getElementById("reset").innerHTML = error;
+        // timeout https://stackoverflow.com/questions/24849/is-there-some-way-to-introduce-a-delay-in-javascript
+        setTimeout(function(){location.reload()},3000);
     }
     return 0;
 }
